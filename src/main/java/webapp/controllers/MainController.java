e8e640ad553c8230e5adca54f35f261c14580fbc
ArtemGrig <artemgrigoryun04@gmail.com>
2024-04-16 17:45:59 +0300

    я

--- src/main/java/webapp/controllers/MainController.java
+++ src/main/java/webapp/controllers/MainController.java
@@ -8,10 +8,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
-    @GetMapping("/")
-    public String home(Model model) {
-        model.addAttribute("tittle", "Главная страница");
-        return "home";
+    @GetMapping("/registration")
+    public String registration(Model model) {
+        model.addAttribute("tittle", "Регистрация");
+        return "registration";
     }
-
+    @GetMapping("/")
+    public String main(Model model) {
+        model.addAttribute("main", "Главная страница");
+        return "main";
+    }
+    @GetMapping("/about")
+    public String about(Model model) {
+        model.addAttribute("about", "cтраница про нас");
+        return "about";
+    }
+    @GetMapping("/selection")
+    public String selection(Model model) {
+        model.addAttribute("selection", "подбор");
+        return "selection";
+    }
 }
