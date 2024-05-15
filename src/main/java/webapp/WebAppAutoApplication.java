a70d8cf2f648c356ec5ed40d57f1833cbc356485
Avelina <A.niki291004@gmail.com>
2024-05-15 17:30:11 +0300

    User model

--- src/main/java/webapp/WebAppAutoApplication.java
+++ src/main/java/webapp/WebAppAutoApplication.java
@@ -3,6 +3,8 @@ package webapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+
 @SpringBootApplication
 public class WebAppAutoApplication {
 
